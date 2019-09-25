2c91ae7c4a98a1e53cdac49ee52db8ec5ad7646b
Patrick Nee <pnetsmail-ms@yahoo.com>
2019-09-25 22:02:25 +0200

    add sessionCancelled

--- src/routes/webendpoints.js
+++ src/routes/webendpoints.js
@@ -57,4 +57,15 @@ router.post('/savePurchaseRequest/', async (req, res, next) => {
         });
 });
 
+router.post('/sessionCancelled/', async (req, res, next) => {
+    webendpointsObj.sessionCancelled(req)
+        .then(function(result) {
+            res.json(result);
+        })
+        .catch(function (error) {
+            if (!('statusCode' in error)) { error.statusCode = 500; }
+            res.status(error.statusCode).json(error);
+        });
+});
+
 module.exports = router;
