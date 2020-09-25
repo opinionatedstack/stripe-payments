72ae10223ef665d260b514c68feb294e55c68351
Patrick N <pnetsmail-ms@yahoo.com>
2020-09-25 17:53:31 +0200

    update a bunch of env entries

--- src/objects/paymentsMongoDb.js
+++ src/objects/paymentsMongoDb.js
@@ -1,11 +1,15 @@
 const { MongoClient } = require('mongodb');
-const mongoDbClient = new MongoClient(process.env.MONGODB_PAYMENTS_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const mongoDbClient = new MongoClient(
+    process.env.LOGGING_MONGODB_CLUSTER_CONN_STRING + process.env.LOGGING_MONGODB_DB + process.env.LOGGING_MONDODB_OPTIONS,
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
 let db;
 mongoDbClient.connect()
     .then(async r => {
         console.log('Connected correctly to MongoDb server');
-        module.exports.db = await mongoDbClient.db(process.env.MONGODB_PAYMENTS_NAME);
-        console.log('Connected correctly to MongoDb database', process.env.MONGODB_PAYMENTS_NAME);
+        module.exports.db = await mongoDbClient.db(process.env.LOGGING_MONGODB_PAYMENTS_COLLECTION);
+        console.log('Connected correctly to MongoDb database', process.env.LOGGING_MONGODB_PAYMENTS_COLLECTION);
     }, e => {
         console.error('Error connecting to MongoDb server or db', e);
     });
